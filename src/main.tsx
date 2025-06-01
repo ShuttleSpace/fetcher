be040b581c2a7e513bab0a59aa975d816515996d
humphrey <dang8080@qq.com>
2025-06-01 11:57:06 +0800

    init repo

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import { fetch as tauriFetch } from "@tauri-apps/plugin-http";
+window.fetch = tauriFetch;
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
